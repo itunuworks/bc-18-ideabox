817f54063b80f4fd952573798dfb8ef477b59e39
Itunu Fatoki <itunuworks@gmail.com>
2017-01-17 13:40:09 +0100

    Project status after initial implementation of loginPage and basic landingPage

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var viewsPath = __dirname + '/views/';
+
+router.use(function(req, res, next){
+	console.log('/' + req.method);
+	next();
+})
+
+router.get('/', function(req, res){
+	res.sendFile(viewsPath + 'loginpage.html');
+})
+
+router.get('/landingpage', function(req, res){
+	res.sendFile(viewsPath + 'landingpage.html');
+})
+
+app.use('/', router);
+
+app.use('*', function(req, res){
+	res.sendFile(viewsPath + '404.html');
+})
+
+app.listen(3000, function(){
+	console.log("Listening for connections to port 3000");
+})
