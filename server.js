e9a1555244ecaaa2331b1fb3bf76d9b97d899664
Itunu Fatoki <itunuworks@gmail.com>
2017-01-17 14:48:02 +0100

    Incorporated code in server.js to initialize firebase.

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var viewsPath = __dirname + '/views/';
+var firebase = require("firebase");
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyC3-cs9GS1LtZgp0mG0uyvDCyTbKco3ZNI",
+	authDomain: "ideabox-dffe8.firebaseapp.com",
+	databaseURL: "https://ideabox-dffe8.firebaseio.com",
+	storageBucket: "ideabox-dffe8.appspot.com",
+	messagingSenderId: "1041486972571"
+};
+
+firebase.initializeApp(config);
+
 
 router.use(function(req, res, next){
 	console.log('/' + req.method);
