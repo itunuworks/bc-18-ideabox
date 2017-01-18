024afc834f1ee3e3f7941220e5348d6c289e8b20
Itunu Fatoki <itunuworks@gmail.com>
2017-01-18 11:46:56 +0100

    Implemented sucessfully user account creation

--- server.js
+++ server.js
@@ -2,18 +2,6 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var viewsPath = __dirname + '/views/';
-var firebase = require("firebase");
-
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyC3-cs9GS1LtZgp0mG0uyvDCyTbKco3ZNI",
-	authDomain: "ideabox-dffe8.firebaseapp.com",
-	databaseURL: "https://ideabox-dffe8.firebaseio.com",
-	storageBucket: "ideabox-dffe8.appspot.com",
-	messagingSenderId: "1041486972571"
-};
-
-firebase.initializeApp(config);
 
 
 router.use(function(req, res, next){
@@ -30,6 +18,8 @@ router.get('/landingpage', function(req, res){
 })
 
 app.use('/', router);
+
+app.use(express.static('lib'));
 
 app.use('*', function(req, res){
 	res.sendFile(viewsPath + '404.html');
