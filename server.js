b04caa2c091ea27d0c2e8317dd937d321db79b0c
Itunu Fatoki <itunuworks@gmail.com>
2017-01-19 16:39:05 +0100

    Project Status after tonnes of modifications. Improvements to UI, Successful user authentication, Complete visual aspect of posting new idea, Markdown implemented in posting of new idea, Success of the logout button and several others

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//require("./lib/main.js");
 var express = require("express");
 var app = express();
 var router = express.Router();
 var viewsPath = __dirname + '/views/';
-
+var authApp;
+// // app.set('views', viewsPath)
+// app.use(express.static(viewsPath))
+// app.set('view engine', 'ejs')
+// console.log(viewsPath);
 
 router.use(function(req, res, next){
 	console.log('/' + req.method);
@@ -10,6 +15,10 @@ router.use(function(req, res, next){
 })
 
 router.get('/', function(req, res){
+	res.sendFile( viewsPath + 'loginpage.html');
+})
+
+router.get('/logout', function(req, res){
 	res.sendFile(viewsPath + 'loginpage.html');
 })
 
@@ -20,6 +29,9 @@ router.get('/landingpage', function(req, res){
 app.use('/', router);
 
 app.use(express.static('lib'));
+app.use(express.static('views'));
+app.use(express.static('css'));
+app.use(express.static('js'));
 
 app.use('*', function(req, res){
 	res.sendFile(viewsPath + '404.html');
@@ -28,3 +40,4 @@ app.use('*', function(req, res){
 app.listen(3000, function(){
 	console.log("Listening for connections to port 3000");
 })
+
